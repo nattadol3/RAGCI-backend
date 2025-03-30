f93193aea56af4d9f30af70e52f30763a79479b4
nattadol3 <119644789+nattadol3@users.noreply.github.com>
2025-03-30 21:44:25 +0700

    Update api.py

--- api.py
+++ api.py
@@ -54,7 +54,7 @@ def get_image(query: str = Query(...)):  # ใช้ Query จาก fastapi
 # Run the API
 if __name__ == "__main__":
     # Get the port from environment variable or default to 8000
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))
     
     # Run the app with the specified host and port
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
