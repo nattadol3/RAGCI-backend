391b4fdb67881a5775b6f8003f58994196aa106c
nattadol3 <119644789+nattadol3@users.noreply.github.com>
2025-03-30 21:34:55 +0700

    Update api.py

--- api.py
+++ api.py
@@ -53,4 +53,4 @@ def get_image(query: str = Query(...)):  # ใช้ Query จาก fastapi
 # Run the API
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
